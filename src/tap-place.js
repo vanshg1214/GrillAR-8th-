2eea79db303feca3aa54ee4bc63c12f9fc24bd6e
Sujal Gupta <avrsmain@gmail.com>
2026-06-03 01:18:45 +0530

    Increase model scale by 8x per user request

--- src/tap-place.js
+++ src/tap-place.js
@@ -14,7 +14,7 @@ export const tapPlaceComponent = {
     
     // Scale config: Setting this to 0.7 makes it 30% smaller than before.
     // Fixed size: We will now ignore manual scaling gestures to keep this size constant.
-    const finalScale = 0.7 
+    const finalScale = 5.6 
 
     // Listeners for gestures
     this._touches = new Map()
